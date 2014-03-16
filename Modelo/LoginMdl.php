d39155972193a65e2bc980a77422e5dbd0320554
agustinRO <jrorozco92@yahoo.com.mx>
2014-03-15 19:36:21 -0600

    Modelo del Login para sus consultas

---
+++ Modelo/LoginMdl.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+	 * @author Javier Rizo Orozco
+	 * Model for the login
+	 */
+	 class LoginModel{
+	 	public $conexion;
+		
+		function __construct(){
+			//Create the conection to the database
+		}
+		
+		function connect($p, $codigo){
+		
+		
+			return true;//We are return a true for this time
+		}
+	 }
+	 
+?>
