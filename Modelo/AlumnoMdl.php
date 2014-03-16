9f99e15e9de2e8e350c87703a0c7c36128b65d90
agustinRO <jrorozco92@yahoo.com.mx>
2014-03-15 19:32:52 -0600

    Agregue Funcion de baja del alumno

--- Modelo/AlumnoMdl.php
+++ Modelo/AlumnoMdl.php
@@ -26,6 +26,12 @@
 			return true; //For the first advance we suppose to think that the data is correct and can been inserted
 						 //The next advance we return a false and we call a sql_command to verify if the data can be inserted or not.
 		}
+		
+		function baja($codigo){//Function to call a query with UPDATE into the database
+		
+			return true;//For the first advance we suppose to think that the code is correct and the studen its down
+		}
+		
 		///Modificacion de Jesus
 		function consultar($codigo){
 			return true;//For the first advance we suppose that the data is correct and can access to the database
